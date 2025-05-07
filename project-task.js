9101cf5331e8d9dadd9f9d8763cec265636eb8e7
Cole Sexton <colesaudio88@gmail.com>
2025-05-07 14:04:18 -0500

    This is my completed work for Practice: Debugging JSON Travel Bookings

--- project-task.js
+++ project-task.js
@@ -47,28 +47,53 @@ but it contains multiple syntax errors that prevent it from being parsed.
 const invalidBookingJSON = `
 {
   "hotelName": "Grand City Hotel",
-  "checkInDate": "2024-05-15"
+  "checkInDate": "2024-05-15", //added a comma
   "checkOutDate": "2024-05-20",
   "guests": [
     {
-      name: "Alice Johnson",
+      "name": "Alice Johnson", //added quotations to name
       "age": 30,
       "email": "alice.johnson@example.com"
     },
     {
       "name": "Bob Smith",
-      "age": undefined,
+      "age": null,   //changed undefined to null, invalid data type
       "email": "bob.smith@example"
     }
   ],
   "roomDetails": {
     "type": "Suite",
     "pricePerNight": 200,
-    "amenities": ["WiFi", "Breakfast", "Parking",]
+    "amenities": ["WiFi", "Breakfast", "Parking"] //removed comma after parking(extra comma)
   }
 }
 `;
 
+/* This is what I pasted into JSON lint and it came out JSON is Valid`
+{
+  "hotelName": "Grand City Hotel",
+  "checkInDate": "2024-05-15", 
+  "checkOutDate": "2024-05-20",
+  "guests": [
+    {
+      "name": "Alice Johnson", 
+      "age": 30,
+      "email": "alice.johnson@example.com"
+    },
+    {
+      "name": "Bob Smith",
+      "age": null,   
+      "email": "bob.smith@example"
+    }
+  ],
+  "roomDetails": {
+    "type": "Suite",
+    "pricePerNight": 200,
+    "amenities": ["WiFi", "Breakfast", "Parking"] 
+  }
+}
+`
+*/
 
 // ============================================
 // ✅ Requirements
@@ -80,6 +105,11 @@ const invalidBookingJSON = `
   • What was wrong?
   • Why is it a problem in JSON?
   • What did you change to fix it?
+
+    Nothing came up wrong when I used the linter I was able to spot all the errors myself.
+    The linter just said "JSON is valid".
+
+
 */
 
 
@@ -92,10 +122,23 @@ const invalidBookingJSON = `
 
 1️⃣ What tools or techniques did you use to identify the errors?
 
+I looked through each line of the JSON to try and identify errors. I did this a couple times 
+until I found all of them
+
 2️⃣ How did you confirm that your corrected JSON file was valid?
+
+I copied the JSON into JSON linter and check it there. It confirmed it was valid.
 
 3️⃣ Which errors were the most difficult to spot? Why?
 
+The commas in incorrect places or missing were the hardest for me just becuase they
+are small and blend in or I think they are there when they are not.
+
 4️⃣ What strategies can help you avoid these kinds of errors in the future?
    (e.g., syntax highlighting, linters, writing JSON by example)
+
+   I think just using VS code in a JSON file is helpful becuase it automatically
+   highlights the errors. Then double checking it by looking it over. Then triple checking
+   it in the JSON linter. 
+   
 */
